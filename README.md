a95f478052c133937f9e71312f6419922eb04da6
Florian Wisniewski <f.wisniewski@web.de>
2021-03-24 10:41:40 +0100

    24-03-2021: initial

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# CabInvest
-Repository for a paper on investiture mechanisms in (as of now) European Parliaments. More tba.
+# CabInvest - Cabinet Investiture
+
+Repository für die Hausarbeit zu Investiturmechanismen und Dauer von Koalitionsverhandlungen. Datenbasis [ERDDA 2014](https://erdda.org/erd/data-archive/) oder evtl. auf Osteuropa zentriert, weil bessere Variablen für Investitur im "Coalition Government in Central and Eastern Europe" (GIE) vorliegen.
+
+Frage aus Exposé:
+> Welche Auswirkungen hat direkter, formeller parlamentarischer Einfluss durch das Investiturvotum auf die Dauer der Regierungsbildung? Welche Erklärungsansätze gibt es hierfür?
+
+# Messung von formellem Einfluss durch Investiturvotum aus den Daten - wie?
++ 
+
+# Dauer der Regierungsbildung - wie messbar?
++ Zeitlicher Ablauf von Wahldatum bis "cabinet in"?
++ Variable zu bargaining duration (gemessen in # of days) möglich?
+
+
+
+
+
+
+
+
---
+++ tests.R
@@ -0,0 +1,55 @@
+# libraries survival analysis
+library(survival)
+library(survminer)
+library(survMisc)
+library(simPH)
+library(flexsurv)
+library(My.stepwise)
+
+# libraries general
+library(tidymodels)
+library(tidyverse)
+library(rms)
+library(MASS)
+library(corrplot)
+library(lubridate)
+library(Amelia)
+library(xtable)
+library(haven)
+library(foreign)
+library(broom)
+library(psych)
+library(Hmisc)
+library(expss)
+library(rockchalk)
+library(effects)
+library(modeldata)
+library(randomForest)
+library(compare)
+library(psych)
+library(desc)
+library(PerformanceAnalytics)
+library(reshape2)
+library(d3heatmap)
+library(kableExtra)
+library(gtools)
+library(gmodels)
+library(summarytools)
+
+##################################################### import dfs
+
+erdda <- read_dta(file = "/Users/flo/Desktop/data/erdda/erdda_b-version-stata12.dta")
+
+gie <- read_dta(file = "/Users/flo/Desktop/data/gie/Data set (04 Dec 2020) with party codes V14.dta")
+
+
+
+
+
+
+
+
+
+
+
+
